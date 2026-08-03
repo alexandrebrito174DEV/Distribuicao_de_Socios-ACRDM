556801fc9b621840cc5e3e2c426480f93ebd482a
alexandrebrito174DEV <alexandrebrito174@gmail.com>
2026-08-03 19:31:00 +0100

    Add new information

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from fastapi import FastAPI #import necessário - vai importar algo da biblioteca
+from fastapi import FastAPI
+from pydantic import BaseModel
+
 # import random - posso fazer mas é melhor SERIAL na Base de Dados
+
 app=FastAPI() #inicio da api - diz que estamos a querer criar a api
+class UtilizadorACriar (BaseModel):
+
+
+    nome: str
+    perfil: str
+    mensagem: str
+    email: str
+
+class UtilizadorSocio (UtilizadorACriar):
+        id_socio: int
+        telefone: str
+        idade: int
+        morada: str
+
+
 @app.get("/") #rota inicial
 
 def start():
@@ -18,10 +36,10 @@ def sobre():
 #cuidado com a indentação, isto não é C
 
 #-------------------------------------------------------------------------------------------
-@app.get("/socio_id")
-def mostrar():
+@app.get("/socio/{id_socio}")
+def mostrar(id_socio: int):
     return {
-        "id socio": 2,
+        "id socio": id_socio,
         "nome_socio": "Alexandre Dev",
         "email": "xxx@gmail.com",
         "mensagem": "Informações acerca do Sócio estão a ser apresentadas"
@@ -31,11 +49,6 @@ def mostrar():
 @app.get("/socios")
 def mostrar_geral():
 
-    def informacao_mensagem():
-        return {
-            "mensagem_i": "Informação de todos os sócios adicionados foi apresentada no ecrã"
-        }
-    informacao_mensagem() = mensagem_i,
 
     return{
         #socios  - nao deixa em parte de ser especie de um array de dados que consegue agrupar
@@ -55,22 +68,25 @@ def mostrar_geral():
         "email": "www@gmail.com",
         "telefone": "+351 000000000"
             }
-    ]
-        # --- fim de dicionário ---
+    ],
+         "mensagem_i": "Informação de todos os sócios adicionados foi apresentada no ecrã"
+
+    # --- fim de dicionário ---
     }
 
 # "socios" é uma chave do dicionário.
 # O valor dessa chave é uma lista de dicionários,
 # onde cada dicionário representa um sócio.
 
-@app.post("/socio_id")
-def criar_socio():
+@app.post("/socio")
+def criar_socio(Socio: UtilizadorACriar):
     return {
         "id_socio": 1,
-        "nome_socio": "Alexandre",
-        "email": "alexandrebrito174@gmail.com",
-        "telefone": "+351 924023383",
-        "mensagem": "Foi criado um sócio"
+        "nome_socio": Socio.nome,
+        "email": Socio.email,
+        "telefone": Socio.telefone,
+        "mensagem": Socio.mensagem,
+        "perfil": Socio.perfil,
     }
 @app.put("/socios/{id_socio}") #usar um caminho reutilizavel devo usar {id_socio}
 def atualizar_socios(id_socio: int):
@@ -92,8 +108,8 @@ def atualizar_dados(id_socio: int): #id_socio como parametro para tornar reutili
         "nome_socio": "Alexandre - O Grande DEV",
         "email": "alexandrebrito_dev@gmail.com",
         "telefone": "+351 924000000",
-        "Informações atualizadas": "As informações acerca do Sócio {id_socio} foi atualizada"
-    }
+        f"Informações atualizadas": "As informações acerca do Sócio {id_socio} foi atualizada"
+    } #importante colocar o 'f' para em parte chamar e apresentar o valor de determinada variavel
 
 
 #-------------------------------------------------------------------------------
@@ -104,7 +120,8 @@ def admin():
         "id_admin": 1,
         "nome_admin": "Alexandrina Cunha",
         "email": "qqqq@gmail.com",
-        "telefone": "+351 111111111"
+        "telefone": "+351 111111111",
+        "função": "Administradora da gestão de Sócios da ACRDM"
     }
 @app.post("/admin")
 def criar_admin():
@@ -112,7 +129,8 @@ def criar_admin():
         "id_admin": 1,
         "nome_admin": "Alexandrina Cunha",
         "email": "qqqq@gmail.com",
-        "telefone": "+351 111111111"
+        "telefone": "+351 111111111",
+        "função": "Administradora da gestão de Sócios da ACRDM"
     }
 @app.patch("/admin")
 def atualizar_admin():
@@ -120,7 +138,8 @@ def atualizar_admin():
         "id_admin": 1,
         "nome_admin": "Alexandrina Cunha",
         "email": "cccc@gmail.com",
-        "telefone": "+351 111111000"
+        "telefone": "+351 111111000",
+        "função": "Administradora da gestão de Sócios "
     }
 #----------------------------------------------------------------
 
@@ -130,7 +149,8 @@ def mostrar_dev():
     return {
         "id_admin_dev": 1,
         "nome_admin_dev": "Alexandre Brito",
-        "email": "alexandre_dev@acrdm.pt"
+        "email": "alexandre_dev@acrdm.pt",
+        "função": "Administrador da parte técnica de Gestão de Sócios da ACRDM"
     }
 
 @app.post("/admin_dev")
@@ -138,7 +158,8 @@ def criar_dev():
     return {
     "id_admin_dev": 1,
     "nome_admin_dev": "Alexandre Brito",
-    "email": "alexandre_dev@acrdm.pt"
+    "email": "alexandre_dev@acrdm.pt",
+    "função": "Administrador da parte técnica de Gestão de Sócios da ACRDM"
     }
 #-----------------------
 # futuramente a inserção de algum tipo de dados é opcional, como EMAIL
